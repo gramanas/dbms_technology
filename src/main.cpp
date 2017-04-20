5bce931838b21ebc7f020e86fbf8e3ef86079a19
gramanas <anastasis.gramm2@gmail.com>
2017-04-20 20:13:37 +0300

    Modified main to accomodate the other changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/stat.h> 
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h> 
@@ -20,6 +21,7 @@ int main(int argc, char** argv) {
         cout << "print filename [[r]/[b] N]  -- If no arguments are given prints whole file. " << endl;
         cout << "                               r and then a number prints record N" << endl;
         cout << "                               b and then a number pritns the whole block N" << endl;
+        cout << "                               c to count the valid entries" << endl;
     } else if (strcmp(argv[1], "create") == 0) {
         createRandFile(argv[2], atoi(argv[3]));
     } else if (strcmp(argv[1], "print") == 0) {
@@ -29,8 +31,15 @@ int main(int argc, char** argv) {
             printFile(argv[2], (uint)atoi(argv[4]), true, 0, false);
         } else if (strcmp(argv[3], "b") == 0) {
             printFile(argv[2], 0, false, (uint)atoi(argv[4]), true);
+        } else if (strcmp(argv[3], "c") == 0) {
+            cout << countValid(argv[2]) << endl;
         }
-    } else {
+    } else if (strcmp(argv[1], "dupl") == 0) {
+        uint first, second;
+        block_t *buffer = new block_t[20];
+        EliminateDuplicates(argv[2], 1, buffer, 5, argv[3], &first, &second);
+    }
+      else {
         cout << "Type -h for quick help." << endl;
     }
     return 0;
