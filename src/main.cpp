dd3fba35cecd462a4aa3c5e3a9fc7e8980348647
gramanas <anastasis.gramm2@gmail.com>
2017-04-20 22:34:30 +0300

    Added dupl command

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,14 @@ int main(int argc, char** argv) {
                 strcmp(argv[1], "--help") == 0) {
         cout << "Usage:\n" << endl;
         cout << "create filename N -- Creates a random binary file named \"filename\" with N blocks." << endl;
-        cout << "print filename [[r]/[b] N]  -- If no arguments are given prints whole file. " << endl;
+        cout << "print filename [c/[[r]/[b] N]]  -- If no arguments are given prints whole file. " << endl;
         cout << "                               r and then a number prints record N" << endl;
         cout << "                               b and then a number pritns the whole block N" << endl;
-        cout << "                               c to count the valid entries" << endl;
+        cout << "                               c to count the valid entries" << endl << endl;
+        cout << "dupl infile field #memory_blocks outfile" << endl;
+        cout << "                               This eliminates duplciates in the field specified" << endl;
+        cout << "                               The field can be [record, number, string, numstr]" << endl;
+        cout << "                               #memory_blocks is an integer" << endl;
     } else if (strcmp(argv[1], "create") == 0) {
         createRandFile(argv[2], atoi(argv[3]));
     } else if (strcmp(argv[1], "print") == 0) {
@@ -36,8 +40,18 @@ int main(int argc, char** argv) {
         }
     } else if (strcmp(argv[1], "dupl") == 0) {
         uint first, second;
+        char field = 4; // wrong field in case user enters wrong name
         block_t *buffer = new block_t[20];
-        EliminateDuplicates(argv[2], 1, buffer, 5, argv[3], &first, &second);
+        if (strcmp(argv[3], "record") == 0) {
+            field = '0';
+        } else if (strcmp(argv[3], "number") == 0) {
+            field = '1';
+        } else if (strcmp(argv[3], "string") == 0) {
+            field = '2';
+        } else if (strcmp(argv[3], "numstr") == 0) {
+            field = '3';
+        }
+        EliminateDuplicates(argv[2], field, buffer, (uint)atoi(argv[4]), argv[5], &first, &second);
     }
       else {
         cout << "Type -h for quick help." << endl;
