cff1af8d3a0f222e079c06ed0f9c40f5a54eb2e1
gramanas <anastasis.gramm2@gmail.com>
2017-04-20 21:26:50 +0300

    Replaced fcntl with stdio.h and files with stream objects
    Also case 0 and 1 for EliminateDuplicates is ready

--- src/dbtproj.cpp
+++ src/dbtproj.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h> 
-#include <fcntl.h> 
 #include <unistd.h>
 #include <list>
 #include <algorithm>
+#include <iterator>
 
 #include "./dbtproj.h"
 
@@ -14,17 +14,24 @@ using namespace std;
 void MergeSort (char *infile, unsigned char field,
                 block_t *buffer, unsigned int nmem_blocks,
                 char *outfile, unsigned int *nsorted_segs,
-                unsigned int *npasses, unsigned int *nios) {
-    int file = open(infile, O_RDONLY, S_IRWXU);
-    if (file == -1) {
-        cerr << "No such file." << endl;
-        exit(1);
+                unsigned int *npasses, unsigned int *nios) { 
+    FILE *in, *out;
+
+    in = fopen(infile, "r");
+    if (in == NULL) {
+        cerr << "Could not open input file." << endl;
     }
 
+    out = fopen(outfile, "w");
+    if (out == NULL) {
+        cerr << "Could not open output file." << endl;
+    }
 
-    //TODO
-    close(file);
     
+    //TODO
+
+    fclose(in);
+    fclose(out);    
 }
 //   field: which field will be used for sorting: 0 is for recid, 1 is for num, 2 is for str and 
 //   3 is for both num and str
@@ -32,53 +39,60 @@ void MergeSort (char *infile, unsigned char field,
 void EliminateDuplicates (char *infile, unsigned char field,
                           block_t *buffer, unsigned int nmem_blocks,
                           char *outfile, unsigned int *nunique, unsigned int *nios) {
-    int file = open(infile, O_RDONLY, S_IRWXU);
-    if (file == -1) {
-        cerr << "No such file." << endl;
-        exit(1);
+    FILE *in, *out;
+
+    in = fopen(infile, "r");
+    if (in == NULL) {
+        cerr << "Could not open input file." << endl;
     }
 
-    cout << outfile << endl;
-
-    int out = creat(outfile, S_IRWXU);
-    if (out == -1) {
-        cerr << "Can't create output file. Permissions?" << endl;
-        exit(1);
+    out = fopen(outfile, "w");
+    if (out == NULL) {
+        cerr << "Could not open output file." << endl;
     }
     
     buffer = new block_t[nmem_blocks];
-    std::list<uint> list;
-    
-    for (uint i = 0; i < nmem_blocks; i++) {
-        read(file, &buffer[i], sizeof(block_t));
-        for (uint j = 0; j < buffer[i].nreserved; j++) { 
-            switch (field) {
-            // case 0:
-                
-            //     break;
-            case 1:
-                // if num exists in list
-                if (find(list.begin(), list.end(), buffer[i].entries[j].num) != list.end()) {
-                    buffer[i].entries[j].valid = false;
-                } else {
-                    list.push_back(buffer[i].entries[j].num);
+    list<uint> numList;
+    list<char *> strList;
+    uint i = 0;
+
+    while(!feof(in)) {
+        while (i < nmem_blocks && !feof(in)) {
+            fread(&buffer[i], 1, sizeof(block_t), in);
+            for (uint j = 0; j < buffer[i].nreserved; j++) { 
+                switch (field) {
+                case 0: // recid
+                    // if num exists in numList
+                    if (find(numList.begin(), numList.end(), buffer[i].entries[j].recid) != numList.end()) {
+                        buffer[i].entries[j].valid = false;
+                    } else {
+                        numList.push_back(buffer[i].entries[j].recid);
+                    }
+                    break;
+                case 1: // num
+                    // if num exists in numList
+                    if (find(numList.begin(), numList.end(), buffer[i].entries[j].num) != numList.end()) {
+                        buffer[i].entries[j].valid = false;
+                    } else {
+                        numList.push_back(buffer[i].entries[j].num);
+                    }
+                    break;
+                    // case 2: // str
+                    //TODO
+                    //     break;
+                    // case 3: // str + num
+                    //TODO
+                    //     break;
+                default:
+                    cerr << "wrong filed" << endl;
+                    exit(1);
                 }
-                break;
-            // case 2:
-                
-            //     break;
-            // case 3:
-                
-            //     break;
-            default:
-                cerr << "wrong filed" << endl;
-                exit(1);
             }
+            fwrite(&buffer[i], 1, sizeof(block_t), out);
         }
-        write(out, &buffer[i], sizeof(block_t));
     }
-    close(file);
-    close(out);
+    fclose(in);
+    fclose(out);
 }
 
 void MergeJoin (char *infile1, char *infile2,
