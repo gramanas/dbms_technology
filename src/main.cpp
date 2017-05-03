67c721e6576133a224930127b303bc23de1582be
gramanas <anastasis.gramm2@gmail.com>
2017-05-04 01:00:27 +0300

    Added sort

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,10 @@ int main(int argc, char** argv) {
         cout << "                               r and then a number prints record N" << endl;
         cout << "                               b and then a number pritns the whole block N" << endl;
         cout << "                               c to count the valid entries" << endl << endl;
+        cout << "sort infile field #memory_blocks outfile" << endl;
+        cout << "                               External Merge Sort of file in filed using #memory_blocks" << endl;
+        cout << "                               The field can be [record, number, string, numstr]" << endl;
+        cout << "                               #memory_blocks is an integer" << endl;
         cout << "dupl infile field #memory_blocks outfile" << endl;
         cout << "                               This eliminates duplciates in the field specified" << endl;
         cout << "                               The field can be [record, number, string, numstr]" << endl;
@@ -36,7 +40,7 @@ int main(int argc, char** argv) {
         } else if (strcmp(argv[3], "b") == 0) {
             printFile(argv[2], 0, false, (uint)atoi(argv[4]), true);
         } else if (strcmp(argv[3], "c") == 0) {
-            cout << countValid(argv[2]) << endl;
+            cout << "Valid records: " << countValid(argv[2]) << endl;
         }
     } else if (strcmp(argv[1], "dupl") == 0) {
         uint nunique, nios;
@@ -54,8 +58,11 @@ int main(int argc, char** argv) {
         EliminateDuplicates(argv[2], field, buffer, (uint)atoi(argv[4]), argv[5], &nunique, &nios);
         cout << "# of unique " << argv[3] << "s in " << argv[5] << ":\n\t" << nunique << "\n\n";
         cout << "# of I/Os in " << argv[5] << ":\n\t" << nios << "\n\n";
+    } else if (strcmp(argv[1], "sort") == 0) {
+        uint nunique, nios, third;
+        MergeSort(argv[2], 'a', new block_t, (uint)atoi(argv[4]), argv[5], &nunique, &nios, &third);
     }
-      else {
+    else {
         cout << "Type -h for quick help." << endl;
     }
     return 0;
