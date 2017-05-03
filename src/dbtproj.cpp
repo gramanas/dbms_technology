60c46a555f3a35caa9079d43c5383eb59ab7ba42
gramanas <anastasis.gramm2@gmail.com>
2017-05-04 01:00:40 +0300

    Begin sort, everything is broken!!!

--- src/dbtproj.cpp
+++ src/dbtproj.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <string.h>
 #include <unistd.h>
 #include <string>
@@ -14,28 +14,158 @@ using namespace std;
 //   field: which field will be used for sorting: 0 is for recid, 1 is for num, 2 is for str and 
 //   3 is for both num and 
 
+void RecordSwap(record_t *x, record_t *y) {
+    record_t temp;
+    temp.num = y->num;
+    temp.rdummy1 = y->rdummy1;
+    temp.rdummy2 = y->rdummy2;
+    temp.rdummy3 = y->rdummy3;
+    temp.recid = y->recid;
+    temp.valid = y->valid;
+    strcpy(temp.str, y->str);
+
+    y->num = x->num;
+    y->rdummy1 = x->rdummy1;
+    y->rdummy2 = x->rdummy2;
+    y->rdummy3 = x->rdummy3;
+    y->recid = x->recid;
+    y->valid = x->valid;
+    strcpy(y->str, x->str);
+    
+    x->num = temp.num;
+    x->rdummy1 = temp.rdummy1;
+    x->rdummy2 = temp.rdummy2;
+    x->rdummy3 = temp.rdummy3;
+    x->recid = temp.recid;
+    strcpy(x->str, temp.str);
+    x->valid = temp.valid;
+}
+
+void QuickSort(block_t *buffer, uint left, uint right) {
+    uint i = left, j = right;
+    uint pivot = left + (right - left) / 2;
+
+    while (i <= j) {
+        if (j > right) return;
+        while (buffer->entries[i].num < buffer->entries[pivot].num) {
+            i++;
+        }
+        while (buffer->entries[pivot].num < buffer->entries[j].num) {
+            j--;
+        }
+        if (i <= j) {
+            RecordSwap(&(buffer->entries[i]), &(buffer->entries[j]));
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j && j <= right) {
+        QuickSort(buffer, left, j);
+    }
+    if (i < right) {
+        QuickSort(buffer, i, right);
+    }
+}
+
+void Sort(block_t *buffer) {
+    uint left = 0;
+    uint right = buffer->nreserved - 1;
+
+    QuickSort(buffer, left, right);
+}
+
+void WriteValidRecords(FILE *outfile, block_t *buffer, uint *nios) {
+    for (uint i = 0; i < buffer->nreserved; i++) {
+        if (buffer->entries[i].valid) {
+            fwrite(&(buffer->entries[i]), 1, sizeof(record_t), outfile);
+            //fseek(outfile, -sizeof(record_t), SEEK_CUR);
+            record_t tmp;
+            fread(&tmp, 1, sizeof(record_t), outfile);
+            cout << tmp.recid << endl;
+            cout << tmp.num << endl;
+            cout << tmp.str << endl;
+            ++*nios;
+        }
+    }
+}
+
+void Invalidate(block_t * buffer) {
+    for (uint i = 0; i < buffer->nreserved; i++) {
+        buffer->entries[i].valid = false;
+    }
+}
+
 void MergeSort (char *infile, unsigned char field,
                 block_t *buffer, unsigned int nmem_blocks,
                 char *outfile, unsigned int *nsorted_segs,
                 unsigned int *npasses, unsigned int *nios) { 
     FILE *in, *out;
 
-    in = fopen(infile, "r");
+    in = fopen(infile, "rb");
     if (in == NULL) {
         cerr << "Could not open input file." << endl;
     }
 
-    out = fopen(outfile, "w");
+    out = fopen(outfile, "wb");
     if (out == NULL) {
         cerr << "Could not open output file." << endl;
     }
 
+    // This is external Merge Sort!
+    // 1. Read nmem_blocks from infile
+    // 2. sort them and save to temp file
+    // 3. repeat till infile is done for
+    // 4. Merge the temp files.
+    
+    // This is external Merge Sort!
+    // 1. Read nmem_blocks from infile
+    // 2. sort each block and merge them all to a temp file
+    // 3. repeat 1-2 till infile is done for
+    // 4. Merge the temp files.
     
     //TODO
+    // May need to define more FOPEN_MAX
+
+
+    size_t n = sizeof(block_t);
+    int tempNum = 0;
+    block_t tempBlk;
+    while(sizeof(block_t) == fread(&tempBlk, 1, sizeof(block_t), in)) {
+        fseek(in, -sizeof(block_t), SEEK_CUR);
+        buffer = new block_t[nmem_blocks];
+        for (uint i = 0; i < nmem_blocks; i++) {
+            n = fread(&buffer[i], 1, sizeof(block_t), in);
+            if (n != sizeof(block_t)) {
+                break;
+            }
+        }
+        for (uint i = 0; i < nmem_blocks; i++) {
+            if (&buffer[i] == NULL) {
+                break;
+            }
+            Sort(&buffer[i]);
+        }
+
+        FILE * temp;
+        temp = fopen(("temp" + to_string(tempNum)).c_str(), "rwb");
+
+        for (uint i = 0; i < nmem_blocks; i++) {
+            fwrite(&buffer[i], 1, sizeof(block_t), temp);
+            //WriteValidRecords(temp, &buffer[i], nios);
+            //Invalidate(&buffer[i]);
+        }
+        fclose(temp);
+
+        tempNum++;
+        free(buffer);
+    }
 
     fclose(in);
     fclose(out);    
 }
+
+
 
 // This fuction performs binary search on a given vector. The only limitation
 // is that the vector type must have comparison operators working (<, <=, ==)
@@ -87,12 +217,12 @@ void EliminateDuplicates (char *infile, unsigned char field,
     FILE *in, *out;
 
     // Files
-    in = fopen(infile, "r");
+    in = fopen(infile, "rb");
     if (in == NULL) {
         cerr << "Could not open input file." << endl;
     }
 
-    out = fopen(outfile, "w");
+    out = fopen(outfile, "wb");
     if (out == NULL) {
         cerr << "Could not open output file." << endl;
     }
@@ -160,10 +290,15 @@ void EliminateDuplicates (char *infile, unsigned char field,
                 }
             }
             // write file
+            // TODO: Here it should write only valid entries,
+            // not the whole block
             fwrite(&buffer[i], 1, sizeof(block_t), out);
-            ++*nios;
+            //WriteValidRecords(out, &buffer[i], nios);
         }
     }
+    cout << "Vector size: " << numVect.size()*sizeof(buffer[1].entries[1].str) << endl;
+    cout << "Buffer size: " << sizeof(block_t)*nmem_blocks << endl;
+    cout << "Buffer2 size: " << sizeof(*buffer) << endl;
     fclose(in);
     fclose(out);
 }
